940943fd00c813bde2205028ace34418f3aa3cc2
Adenildo Junior <adenildojunior52@gmail.com>
2021-02-27 19:03:45 -0300

    :refactor: Organize the pipeline functions and the api functions

--- app.py
+++ app.py
@@ -1,61 +1,19 @@
 from flask import Flask, request, jsonify
-from joblib import load
-from sklearn.feature_extraction.text import TfidfVectorizer
-from unidecode import unidecode
-import spacy
-import re
-import numpy as np
-
+import pipeline as pipeline
 app = Flask(__name__)
 
-# load the classifier model
-pipeline = load("text_classification.joblib")
 
-# NLP Pre process
-def nlp_preprocess(text):
-    
-    lemmas = []
-
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(text)
-    
-    for token in doc:
-        if not token.is_stop: 
-            if (token.pos_ == 'VERB' or token.pos_ == 'NOUN' or token.pos_ == 'ADJ'):
-                lemmas.append(token.lemma_)
-    
-    return clean_text(' '.join(lemmas))
-
-def clean_text(text):
-    text = text.lower()
-    t = unidecode(text)
-    t.encode("ascii")  
-    t = re.sub(r'[^a-z]', ' ', t)           #Remove nonalpha
-    t = re.sub(r'\s[^a-z]\s', ' ', t)       #Remove nonalpha >> check if is really necessary!?!?
-    t = re.sub(r"\b[a-z]{1,2}\b", ' ', t)   #Remove words with 1 or 2 letters
-    t = re.sub(' +', ' ', t)                #Remove extra spaces
-    t = t.strip()                           #Remove leading and trailing spaces
-    return t
-
-
-@app.route('/', methods=["GET"])
+@app.route('/predict', methods=["POST", "GET"])
 def call_model():
     req = request.args
-
-    lemmas = nlp_preprocess(req.get('action'))
-    predict = pipeline.predict([lemmas])
-    prob = pipeline.predict_proba([lemmas])
-
-    best_n = np.argsort(prob, axis=1)[:,-3:]
-    classes = pipeline.classes_
+    print(req.get('action'))
+    prediction, predictions_list = pipeline.predict(req.get('action'))
 
     result_dict = {
         "player": req.get('player'),
         "action": req.get('action'),
-        "prediction": predict[0],#{"Acrobatics": "32.54%"},
-        "other_predicts": [{classes[best_n[0, 2]]: prob[0, best_n[0, 2]]}, 
-        {classes[best_n[0, 1]]: prob[0, best_n[0, 1]]}, 
-        {classes[best_n[0, 0]]: prob[0, best_n[0, 0]]}]
+        "prediction": prediction,
+        "predictions_list": predictions_list
     }
     return jsonify(result_dict)
 
---
+++ pipeline.py
@@ -0,0 +1,49 @@
+from unidecode import unidecode
+import spacy
+import re
+from joblib import load
+import numpy as np
+
+model = load("models/text_classification.joblib")
+
+
+def nlp_preprocess(text):
+    lemmas = []
+
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+
+    for token in doc:
+        if not token.is_stop:
+            if token.pos_ == 'VERB' or token.pos_ == 'NOUN' or token.pos_ == 'ADJ':
+                lemmas.append(token.lemma_)
+
+    return clean_text(' '.join(lemmas))
+
+
+def clean_text(text):
+    text = text.lower()
+    t = unidecode(text)
+    t.encode("ascii")
+    t = re.sub(r'[^a-z]', ' ', t)  # Remove nonalpha
+    t = re.sub(r'\s[^a-z]\s', ' ', t)  # Remove nonalpha >> check if is really necessary!?!?
+    t = re.sub(r"\b[a-z]{1,2}\b", ' ', t)  # Remove words with 1 or 2 letters
+    t = re.sub(' +', ' ', t)  # Remove extra spaces
+    t = t.strip()  # Remove leading and trailing spaces
+    return t
+
+
+def predict(message):
+    lemmas = nlp_preprocess(message)
+    predict = model.predict([lemmas])
+    prob = model.predict_proba([lemmas])
+
+    best_n = np.argsort(prob, axis=1)[:, -3:]
+    classes = model.classes_
+
+    prediction = predict[0]
+    predictions_list = [{classes[best_n[0, 2]]: f"{prob[0, best_n[0, 2]] * 100}%"},
+                        {classes[best_n[0, 1]]: f"{prob[0, best_n[0, 1]] * 100}%"},
+                        {classes[best_n[0, 0]]: f"{prob[0, best_n[0, 0]] * 100}%"}]
+
+    return prediction, predictions_list
